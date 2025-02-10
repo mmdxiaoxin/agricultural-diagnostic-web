26e5d2aeab5d810996c9d5366fe20fc981fc08e4
小新 <782446723@qq.com>
2025-02-10 15:47:24 +0800

    build: 配置vite打包

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,67 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import { ConfigEnv, defineConfig, loadEnv, UserConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
+import { wrapperEnv } from "./src/build/getEnv";
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [react()]
+export default defineConfig((mode: ConfigEnv): UserConfig => {
+	const env = loadEnv(mode.mode, process.cwd());
+	const viteEnv = wrapperEnv(env);
+
+	return {
+		resolve: {
+			alias: {
+				"@": resolve(__dirname, "./src")
+			}
+		},
+		server: {
+			host: "0.0.0.0",
+			port: viteEnv.VITE_PORT,
+			open: viteEnv.VITE_OPEN,
+			cors: true,
+			// https: false,
+			// 代理跨域（mock 不需要配置，这里只是个事列）
+			proxy: {
+				"/api": {
+					target: "http://localhost:5000",
+					changeOrigin: true,
+					rewrite: path => path.replace(/^\/api/, "")
+				}
+			}
+		},
+		plugins: [
+			react(),
+			createHtmlPlugin({
+				inject: {
+					data: {
+						title: viteEnv.VITE_GLOB_APP_TITLE
+					}
+				}
+			})
+		],
+		esbuild: {
+			pure: viteEnv.VITE_DROP_CONSOLE ? ["console.log", "debugger"] : []
+		},
+		build: {
+			outDir: "dist",
+			// esbuild 打包更快，但是不能去除 console.log，去除 console 使用 terser 模式
+			minify: "esbuild",
+			// minify: "terser",
+			// terserOptions: {
+			// 	compress: {
+			// 		drop_console: viteEnv.VITE_DROP_CONSOLE,
+			// 		drop_debugger: true
+			// 	}
+			// },
+			rollupOptions: {
+				output: {
+					// Static resource classification and packaging
+					chunkFileNames: "assets/js/[name]-[hash].js",
+					entryFileNames: "assets/js/[name]-[hash].js",
+					assetFileNames: "assets/[ext]/[name]-[hash].[ext]"
+				}
+			}
+		}
+	};
 });
