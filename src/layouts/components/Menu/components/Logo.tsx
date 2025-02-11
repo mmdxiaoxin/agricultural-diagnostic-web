b82799fe718d0fffe31d290f8b57d9cf1ee64a56
小新 <782446723@qq.com>
2025-02-11 11:13:37 +0800

    fix: menu header 汉化

--- src/layouts/components/Menu/components/Logo.tsx
+++ src/layouts/components/Menu/components/Logo.tsx
@@ -7,7 +7,7 @@ const Logo = () => {
 	return (
 		<div className="logo-box">
 			<img src={logo} alt="logo" className="logo-img" />
-			{!isCollapse ? <h2 className="logo-text">Argi Diag</h2> : null}
+			{!isCollapse ? <h2 className="logo-text">智能诊断系统</h2> : null}
 		</div>
 	);
 };
