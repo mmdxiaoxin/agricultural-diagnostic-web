a15b1d266063f73ac334f15b5c11b55dec62ee3e
小新 <782446723@qq.com>
2025-02-12 18:04:33 +0800

    refactor: 重构样式表

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter } from "react-router";
 import Routes from "./routes";
 import AuthRouter from "./routes/utils/authRouter";
-import "@/styles/theme/theme-default.scss";
 
 const App = () => {
 	return (
