06004aa3c64310966adabba711c77cbc490ee089
小新 <782446723@qq.com>
2025-02-20 13:02:11 +0800

    feat: 添加数据集管理界面，优化样式和布局

--- src/components/DatasetsList/index.tsx
+++ src/components/DatasetsList/index.tsx
@@ -1,18 +1,16 @@
-import { DatasetMeta, ReqUpdateDataset } from "@/api/interface";
-import { deleteDataset, getDatasetsList, updateDataset } from "@/api/modules/file";
+import { DatasetMeta } from "@/api/interface";
+import { deleteDataset, getDatasetsList } from "@/api/modules/file";
 import { formatSize } from "@/utils";
 import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
-import { Button, Form, Input, List, message, Modal, Popconfirm, Tag } from "antd";
+import { Button, Card, List, message, Popconfirm, Tag } from "antd";
 import React, { useEffect, useState } from "react";
+import styles from "./index.module.scss";
 
-export type DatasetsListProps = {};
+export interface DatasetsListProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-const DatasetsList: React.FC<DatasetsListProps> = () => {
-	const [form] = Form.useForm();
-
+const DatasetsList: React.FC<DatasetsListProps> = props => {
 	const [loading, setLoading] = useState<boolean>(false);
 	const [datasets, setDatasets] = useState<DatasetMeta[]>([]);
-	const [currentDataset, setCurrentDataset] = useState<DatasetMeta | null>(null);
 
 	const fetchListData = async () => {
 		setLoading(true);
@@ -44,84 +42,48 @@ const DatasetsList: React.FC<DatasetsListProps> = () => {
 		}
 	};
 
-	const handleSave = async (values: ReqUpdateDataset) => {
-		try {
-			if (!currentDataset) return;
-
-			await updateDataset(currentDataset.id, values);
-			await fetchListData();
-
-			setCurrentDataset(null);
-			message.success("数据集更新成功");
-		} catch (error) {
-			message.error("数据集更新失败");
-		}
-	};
-
 	return (
-		<>
+		<div {...props}>
 			<List
 				loading={loading}
 				itemLayout="vertical"
 				dataSource={datasets}
+				grid={{ xxl: 3, xl: 2, lg: 2, md: 2, sm: 1, xs: 1 }}
 				renderItem={dataset => (
-					<List.Item
-						key={dataset.id}
-						actions={[
-							<Button
-								type="link"
-								icon={<EditOutlined />}
-								onClick={() => setCurrentDataset(dataset)}
-							>
-								编辑
-							</Button>,
-							<Popconfirm
-								title="确认删除?"
-								description="删除数据集后不可恢复"
-								onConfirm={() => handleDelete(dataset.id)}
-								okText="确认"
-								cancelText="取消"
-							>
-								<Button type="link" danger icon={<DeleteOutlined />}>
-									删除
-								</Button>
-							</Popconfirm>
-						]}
-					>
-						<List.Item.Meta title={dataset.name} description={dataset.description} />
-						<div>
-							<Tag color={"green"}>文件总数: {dataset.file_count}</Tag>
-							<span>{formatSize(dataset.dataset_size)}</span>
-						</div>
-					</List.Item>
+					<Card key={dataset.id} className={styles["list-item"]} hoverable>
+						<List.Item
+							actions={[
+								<Button type="link" icon={<EditOutlined />}>
+									编辑
+								</Button>,
+								<Popconfirm
+									title="确认删除?"
+									description="删除数据集后不可恢复"
+									onConfirm={() => handleDelete(dataset.id)}
+									okText="确认"
+									cancelText="取消"
+								>
+									<Button type="link" danger icon={<DeleteOutlined />}>
+										删除
+									</Button>
+								</Popconfirm>
+							]}
+						>
+							<List.Item.Meta
+								title={dataset.name}
+								description={
+									<div className={styles["list-item__content"]}>
+										<p>{dataset.description}</p>
+										<Tag color={"green"}>文件总数: {dataset.file_count}</Tag>
+										<span>{formatSize(dataset.dataset_size || 0)}</span>
+									</div>
+								}
+							/>
+						</List.Item>
+					</Card>
 				)}
 			/>
-			<Modal
-				title="编辑数据集"
-				open={!!currentDataset}
-				onOk={() => form.submit()}
-				onCancel={() => setCurrentDataset(null)}
-			>
-				<Form
-					form={form}
-					labelCol={{ span: 6 }}
-					wrapperCol={{ span: 18 }}
-					layout="horizontal"
-					onFinish={handleSave}
-				>
-					<Form.Item label="数据集名称" name="name" initialValue={currentDataset?.name}>
-						<Input />
-					</Form.Item>
-					<Form.Item
-						label="数据集描述"
-						name="description"
-						initialValue={currentDataset?.description}
-					>
-						<Input />
-					</Form.Item>
-				</Form>
-			</Modal>
-		</>
+		</div>
 	);
 };
 
--- src/views/capture/DatasetManage.tsx
+++ src/views/capture/DatasetManage.tsx
@@ -1,9 +1,16 @@
+import DatasetsList from "@/components/DatasetsList";
 import React from "react";
+import styles from "./DatasetManage.module.scss";
 
 export type DatasetManageProps = {};
 
-const DatasetManage: React.FC<DatasetManageProps> = (props: DatasetManageProps) => {
-	return <div>数据集管理</div>;
+const DatasetManage: React.FC<DatasetManageProps> = () => {
+	return (
+		<div className={styles["container"]}>
+			<div className={styles["header"]}>Header</div>
+			<DatasetsList className={styles["content"]} />
+		</div>
+	);
 };
 
 export default DatasetManage;
