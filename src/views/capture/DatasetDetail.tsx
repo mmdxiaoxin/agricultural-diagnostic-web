a99f056eff1564a3c91caa60db18bf1279176849
小新 <782446723@qq.com>
2025-02-20 15:36:15 +0800

    feat: 添加数据集传输组件，支持数据集的选择和管理功能

--- src/views/capture/DatasetDetail.tsx
+++ src/views/capture/DatasetDetail.tsx
@@ -1,13 +1,54 @@
-import React from "react";
+import { getDatasetDetail } from "@/api/modules/file";
+import DatasetTransfer from "@/components/DatasetTransfer";
+import { Col, Form, Input, message, Row } from "antd";
+import React, { useEffect } from "react";
 import { useParams } from "react-router";
+import styles from "./DatasetDetail.module.scss";
 
 export type DatasetDetailProps = {
-	mode: "create" | "edit";
+	mode?: "create" | "edit";
 };
 
-const DatasetDetail: React.FC<DatasetDetailProps> = () => {
+const DatasetDetail: React.FC<DatasetDetailProps> = ({ mode }) => {
 	const { id } = useParams();
-	return <div>DatasetDetail {id}</div>;
+
+	const [form] = Form.useForm();
+
+	const fetchDataset = async (id: number) => {
+		try {
+			const response = await getDatasetDetail(id);
+
+			if (response.code !== 200) throw new Error(response.message);
+
+			form.setFieldsValue(response.data);
+		} catch (error: any) {
+			message.error(error.message);
+		}
+	};
+
+	useEffect(() => {
+		if (mode === "edit" && id) {
+			fetchDataset(Number(id));
+		}
+	}, [id, mode]);
+
+	return (
+		<Row gutter={16} className={styles["container"]}>
+			<Col span={6}>
+				<Form form={form} layout="vertical" initialValues={{ id, name: "", description: "" }}>
+					<Form.Item label="数据集名称" name="name">
+						<Input />
+					</Form.Item>
+					<Form.Item label="数据集描述" name="description">
+						<Input.TextArea />
+					</Form.Item>
+				</Form>
+			</Col>
+			<Col span={18}>
+				<DatasetTransfer />
+			</Col>
+		</Row>
+	);
 };
 
 export default DatasetDetail;
