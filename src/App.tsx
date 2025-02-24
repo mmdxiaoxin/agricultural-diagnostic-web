3e52ad41860292daa616dbc329d553a6338cbe26
小新 <782446723@qq.com>
2025-02-24 15:33:04 +0800

    feat: 添加网络状态监测功能，用户离线时显示错误消息

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { message } from "antd";
+import { useEffect } from "react";
 import { BrowserRouter } from "react-router";
 import Routes from "./routes";
 import AuthRouter from "./routes/utils/authRouter";
 
+const networkStatus = () => {
+	message.error("网络连接已断开");
+};
+
 const App = () => {
+	useEffect(() => {
+		window.addEventListener("offline", networkStatus);
+
+		return () => {
+			window.removeEventListener("offline", networkStatus);
+		};
+	}, []);
 	return (
 		<BrowserRouter>
 			<AuthRouter>
