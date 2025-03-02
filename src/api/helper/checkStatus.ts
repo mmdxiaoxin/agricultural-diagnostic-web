1003bf08ce1f1af782b05d139d6d26691e348b20
小新 <782446723@qq.com>
2025-03-03 00:06:28 +0800

    feat: 修改checkStatus函数以支持自定义错误消息

--- src/api/helper/checkStatus.ts
+++ src/api/helper/checkStatus.ts
@@ -1,43 +1,43 @@
-import { message } from "antd";
+import { message as AntdMessage } from "antd";
 
 /**
  * @description: 校验网络请求状态码
  * @param {Number} status
  * @return void
  */
-export const checkStatus = (status: number): void => {
+export const checkStatus = (status?: number, message?: string | string[]): void => {
 	switch (status) {
 		case 400:
-			message.error("请求失败！请您稍后重试");
+			AntdMessage.error(message ?? "请求失败！请您稍后重试");
 			break;
 		case 401:
-			message.error("登录失效！请您重新登录");
+			AntdMessage.error(message ?? "登录失效！请您重新登录");
 			break;
 		case 403:
-			message.error("当前账号无权限访问！");
+			AntdMessage.error(message ?? "当前账号无权限访问！");
 			break;
 		case 404:
-			message.error("你所访问的资源不存在！");
+			AntdMessage.error(message ?? "你所访问的资源不存在！");
 			break;
 		case 405:
-			message.error("请求方式错误！请您稍后重试");
+			AntdMessage.error(message ?? "请求方式错误！请您稍后重试");
 			break;
 		case 408:
-			message.error("请求超时！请您稍后重试");
+			AntdMessage.error(message ?? "请求超时！请您稍后重试");
 			break;
 		case 500:
-			message.error("服务异常！");
+			AntdMessage.error(message ?? "服务异常！");
 			break;
 		case 502:
-			message.error("网关错误！");
+			AntdMessage.error(message ?? "网关错误！");
 			break;
 		case 503:
-			message.error("服务不可用！");
+			AntdMessage.error(message ?? "服务不可用！");
 			break;
 		case 504:
-			message.error("网关超时！");
+			AntdMessage.error(message ?? "网关超时！");
 			break;
 		default:
-			message.error("请求失败！");
+			AntdMessage.error(message ?? "请求失败！");
 	}
 };
