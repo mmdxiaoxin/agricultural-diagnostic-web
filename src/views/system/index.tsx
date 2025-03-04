35d62cfb93f4200d966c23bb3ef77eaaa287a13c
小新 <782446723@qq.com>
2025-03-04 21:54:34 +0800

    feat: 系统配置页面

--- src/views/system/index.tsx
+++ src/views/system/index.tsx
@@ -1,9 +1,55 @@
-import React from "react";
+import { useAppDispatch, useAppSelector } from "@/hooks";
+import { setThemeConfig } from "@/store/modules/globalSlice";
+import { setCollapse } from "@/store/modules/menuSlice";
+import { SettingOutlined } from "@ant-design/icons";
+import { Divider, Space, Switch } from "antd";
 
-type Props = {};
+const SystemConfig: React.FC = () => {
+	const { themeConfig } = useAppSelector(state => state.global);
+	const { breadcrumb, tabs, footer } = themeConfig;
+	const { isCollapse } = useAppSelector(state => state.menu);
 
-const SystemConfig = (props: Props) => {
-	return <div>SystemConfig</div>;
+	const dispatch = useAppDispatch();
+
+	// 切换配置项
+	const onChange = (checked: boolean, keyName: string) => {
+		dispatch(setThemeConfig({ ...themeConfig, [keyName]: !checked }));
+	};
+
+	// 更新菜单折叠状态
+	const updateCollapse = (checked: boolean) => {
+		dispatch(setCollapse(checked));
+	};
+
+	return (
+		<div className="w-full h-full bg-white p-4 rounded-lg">
+			<Divider>
+				<Space>
+					<SettingOutlined />
+					界面设置
+				</Space>
+			</Divider>
+
+			<div className="space-y-4">
+				<div className="flex justify-between items-center">
+					<span>折叠菜单</span>
+					<Switch checked={isCollapse} onChange={updateCollapse} />
+				</div>
+				<div className="flex justify-between items-center">
+					<span>面包屑导航</span>
+					<Switch checked={!breadcrumb} onChange={e => onChange(e, "breadcrumb")} />
+				</div>
+				<div className="flex justify-between items-center">
+					<span>标签栏</span>
+					<Switch checked={!tabs} onChange={e => onChange(e, "tabs")} />
+				</div>
+				<div className="flex justify-between items-center">
+					<span>页脚</span>
+					<Switch checked={!footer} onChange={e => onChange(e, "footer")} />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default SystemConfig;
