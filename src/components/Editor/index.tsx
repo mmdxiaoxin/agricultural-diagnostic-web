9056f19bac11d1e2b23f1e4750c1cc77e9b9d42c
小新 <782446723@qq.com>
2025-04-17 09:41:41 +0800

    refactor: MonacoEditor简化配置

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -5,31 +5,6 @@ import React, { useCallback, useMemo } from "react";
 // 配置 Monaco Editor 使用本地文件 (CDN较慢)
 loader.config({ monaco });
 
-import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
-import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
-import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
-import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
-import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
-
-// 配置 worker
-self.MonacoEnvironment = {
-	getWorker(_, label) {
-		if (label === "json") {
-			return new jsonWorker();
-		}
-		if (label === "css" || label === "scss" || label === "less") {
-			return new cssWorker();
-		}
-		if (label === "html" || label === "handlebars" || label === "razor") {
-			return new htmlWorker();
-		}
-		if (label === "typescript" || label === "javascript") {
-			return new tsWorker();
-		}
-		return new editorWorker();
-	}
-};
-
 // 编辑器主题类型
 export type EditorTheme = "light" | "vs-dark" | "hc-black";
 
