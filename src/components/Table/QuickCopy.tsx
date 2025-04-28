23aa2cb907751f3fecab1894259e4a1d8bedd89a
小新 <782446723@qq.com>
2025-04-28 21:55:19 +0800

    feat: 添加最大长度属性以支持文本截断和工具提示

--- src/components/Table/QuickCopy.tsx
+++ src/components/Table/QuickCopy.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "antd";
+import { Typography, Tooltip } from "antd";
 import clsx from "clsx";
 import React from "react";
 
@@ -6,18 +6,24 @@ const { Paragraph } = Typography;
 
 export type QuickCopyProps = {
 	text: string;
+	maxLength?: number;
 };
 
-const QuickCopy: React.FC<QuickCopyProps> = ({ text }) => {
-	return (
+const QuickCopy: React.FC<QuickCopyProps> = ({ text, maxLength = 30 }) => {
+	const displayText =
+		maxLength && text.length > maxLength ? `${text.slice(0, maxLength)}...` : text;
+
+	const content = (
 		<Paragraph
 			copyable
 			ellipsis={{ rows: 1, expandable: true }}
 			className={clsx("text-blue-500 cursor-pointer hover:text-blue-600", "!mb-0")}
 		>
-			{text}
+			{displayText}
 		</Paragraph>
 	);
+
+	return text.length > maxLength ? <Tooltip title={text}>{content}</Tooltip> : content;
 };
 
 export default QuickCopy;
